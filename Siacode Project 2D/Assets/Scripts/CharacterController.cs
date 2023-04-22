730da351e66a55b18bb3d7df934c33039144636c
OlegsivDev <olegsiv345@gmail.com>
2023-04-22 20:42:56 +0300

    add arrow shooting system

--- Siacode Project 2D/Assets/Scripts/CharacterController.cs
+++ Siacode Project 2D/Assets/Scripts/CharacterController.cs
@@ -1,34 +1,70 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
+using UnityEngine.UI;
+using Object = System.Object;
 
 public class CharacterController : MonoBehaviour
 {
     public float speed;
     private Rigidbody2D rb;
-
-    private Vector2 _transition;
+    public float rateOfFire;
+    private bool _isShooting;
+    public GameObject projectile;
+    public Vector2 _transition;
+    public float lastZMovingAngle;
+    public float projectileSpawnDistance;
 
     // Start is called before the first frame update
     // Update is called once per frame
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        _isShooting = false;
     }
 
     void Update()
     {
+        
     }
 
     private void FixedUpdate()
     {
         Move();
+        shoot();
     }
 
     private void Move()
     {
-        _transition = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        rb.velocity = (_transition * speed * Time.fixedDeltaTime);
+        
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            _transition.x = Input.GetAxis("Horizontal");
+            _transition.y = Input.GetAxis("Vertical");
+            rb.velocity = (_transition * speed * Time.fixedDeltaTime);
+            lastZMovingAngle = Mathf.Atan2(_transition.x, -1 * Input.GetAxis("Vertical")) * Mathf.Rad2Deg;
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+    private void shoot()
+    {
+        if (Input.GetKey(KeyCode.Space) && !_isShooting)
+        {
+            StartCoroutine(Shooting());
+        }
+    }
+
+    IEnumerator Shooting()
+    {
+        _isShooting = true;
+        Instantiate(projectile, transform.position + new Vector3(_transition.x, _transition.y, 0).normalized * projectileSpawnDistance, Quaternion.Euler(0,0, lastZMovingAngle));
+        yield return new WaitForSeconds(60 / rateOfFire);
+        _isShooting = false;
     }
 }
---
+++ Siacode Project 2D/Assets/Scripts/Projectile.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DestroyObject", 5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void DestroyObject()
+    {
+        Destroy(gameObject);
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position -= transform.up * speed * Time.deltaTime;
+    }
+}
