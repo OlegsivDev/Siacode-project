2ae692c7761ef5bca47b3f5d27106c990cb874d0
OlegsivDev <olegsiv345@gmail.com>
2023-04-24 11:22:48 +0300

    Add camera movement

---
+++ Siacode Project 2D/Assets/Scripts/CameraController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 mousePos;
+    public float cameraDistance;
+    public float cameraMinDistance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveCamera();
+        // TODO: FIX THIS
+        //MoveCameraToPlayer();
+    }
+
+    private void MoveCameraToPlayer()
+    {
+        GetMouseInputOnScreen();
+        if ((mousePos - player.transform.position).magnitude > cameraMinDistance)
+        {
+            transform.position = player.transform.position +
+                (mousePos - player.transform.position).normalized *
+                Mathf.Clamp((mousePos - player.transform.position).magnitude, 0, cameraDistance);
+        } else
+        {
+            transform.position = player.transform.position;
+        }
+    }
+
+    private void GetMouseInputOnScreen()
+    {
+        mousePos = gameObject.GetComponent<Camera>().ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void MoveCamera()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -8);
+    }
+}
