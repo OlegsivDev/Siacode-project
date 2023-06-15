7b27ba84812c3969a6142858602404905af7e713
OlegsivDev <olegsiv345@gmail.com>
2023-06-15 21:50:25 +0300

    Major feat working new input system

--- Siacode Project 2D/Assets/Scripts/CharacterController.cs
+++ Siacode Project 2D/Assets/Scripts/CharacterController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Pool;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -29,11 +30,21 @@ public class CharacterController : MonoBehaviour
     public int builtInPoolingCapacity;
     public int builtInPoolingMaxCapacity;
     public AudioSource runningSound;
-        
+    private PlayerInput playerInput;
+    private PlayerInputActions _playerInputActions;
+    private Coroutine _shootingCoroutine;
+
     // Start is called before the first frame update
     // Update is called once per frame
     void Start()
     {
+        playerInput = GetComponent<PlayerInput>();
+        _playerInputActions = new PlayerInputActions();
+        _playerInputActions.Player.Shoot.performed += OnShootActionStarted;
+        _playerInputActions.Player.Shoot.canceled += OnShootActionEnded;
+        // playerInputActions.Player.Move.performed += Move;
+        _playerInputActions.Player.Enable();
+
         _rb = GetComponent<Rigidbody2D>();
         _isShooting = false;
         _bowPosition = bow.transform.position;
@@ -68,25 +79,19 @@ public class CharacterController : MonoBehaviour
     private void FixedUpdate()
     {
         Move();
-        Shoot();
         // TODO: FIX ENTER IN MAIN MENU
-        CheckExitToMenu();
     }
 
     private void Move()
     {
-
-        
-        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        _direction = _playerInputActions.Player.Move.ReadValue<Vector2>();
+        animator.SetFloat("Horizontal", _direction.x);
+        animator.SetFloat("Vertical", _direction.y);
+        animator.SetFloat("Speed", _direction.sqrMagnitude);
+        if (_direction.x != 0 || _direction.y != 0)
         {
-            _direction.x = Input.GetAxisRaw("Horizontal");
-            _direction.y = Input.GetAxisRaw("Vertical");
             _rb.velocity = (_direction * speed * Time.fixedDeltaTime);
             lastZMovingAngle = Mathf.Atan2(_direction.x, -1 * _direction.y) * Mathf.Rad2Deg;
-
-            animator.SetFloat("Horizontal", _direction.x);
-            animator.SetFloat("Vertical", _direction.y);
-            animator.SetFloat("Speed", _direction.sqrMagnitude);
 
             if (!runningSound.isPlaying)
             {
@@ -101,69 +106,68 @@ public class CharacterController : MonoBehaviour
                 runningSound.Stop();
             }
         }
-
-
     }
 
-    private void Shoot()
+    private void OnShootActionStarted(InputAction.CallbackContext context)
     {
-        if ((Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.Mouse0)) && !_isShooting)
+        // Start the shooting coroutine
+        _shootingCoroutine = StartCoroutine(Shooting());
+    }
+    
+    private void OnShootActionEnded(InputAction.CallbackContext context)
+    {
+        // Stop the shooting coroutine
+        if (_shootingCoroutine != null)
         {
-            StartCoroutine(Shooting());
-            //if (_toMouseZRotation > 0)
-            //{
-            //    bow.transform.rotation = Quaternion.Euler(0, 0, 90);
-            //    Vector3 newPosition = _bowPosition;
-            //    _bowPosition.x = 0.1f;
-            //    bow.transform.position = _bowPosition;
-            //} else
-            //{
-            //    bow.transform.rotation = Quaternion.Euler(0, 0, -90);
-            //    Vector3 newPosition = _bowPosition;
-            //    _bowPosition.x = -0.1f;
-            //    bow.transform.position = _bowPosition;
-            //}
-            //bow.gameObject.SetActive(true);
-        } else
-        {
-            //bow.gameObject.SetActive(false);
+            StopCoroutine(_shootingCoroutine);
+            _shootingCoroutine = null;
         }
     }
 
     private IEnumerator Shooting()
     {
-        _isShooting = true;
-        
+        {
+            while (true)
+            {
+                GetMouseWorldPosition();
+                if (useBuiltInObjectPooling)
+                {
+                    Pool.Get();
+                }
+                else
+                {
+                    Instantiate(projectile, transform.position + _toMouseVector.normalized * projectileSpawnDistance, Quaternion.Euler(0,0, _toMouseZRotation));
+                }
+                yield return new WaitForSeconds(60 / rateOfFire);
+            }
+        }
         // Uncomment to make shooting in moving direction 
         // Instantiate(projectile, transform.position + new Vector3(_direction.x, _direction.y, 0).normalized * projectileSpawnDistance, Quaternion.Euler(0,0, lastZMovingAngle));
         
         // Shooting to mouse direction
-        GetMouseWorldPosition();
-        if (useBuiltInObjectPooling)
-        {
-            Pool.Get();
-        }
-        else
-        {
-            Instantiate(projectile, transform.position + _toMouseVector.normalized * projectileSpawnDistance, Quaternion.Euler(0,0, _toMouseZRotation));
-        }
-        yield return new WaitForSeconds(60 / rateOfFire);
-        _isShooting = false;
+        
+        // if (useBuiltInObjectPooling)
+        // {
+        //     Pool.Get();
+        // }
+        // else
+        // {
+        //     Instantiate(projectile, transform.position + _toMouseVector.normalized * projectileSpawnDistance, Quaternion.Euler(0,0, _toMouseZRotation));
+        // }
+        // yield return new WaitForSeconds(60 / rateOfFire);
+        // _isShooting = false;
     }
 
     private void GetMouseWorldPosition()
     {
-        Vector3 worldMousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 worldMousePosition = mainCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
         _toMouseVector = worldMousePosition - transform.position;
         _toMouseVector.z = 0;
         _toMouseZRotation = Mathf.Atan2(_toMouseVector.x, -1 * _toMouseVector.y) * Mathf.Rad2Deg;
     }
 
-    private void CheckExitToMenu()
+    public void ExitToMenu()
     {
-        if (Input.GetKey(KeyCode.Escape))
-        {
-            SceneManager.LoadScene("MainMenu");
-        };
+        SceneManager.LoadScene("MainMenu");
     }
 }
