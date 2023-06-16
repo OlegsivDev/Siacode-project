b7aeb7a71019a1ffa80c78b84104d551e052787d
OlegsivDev <olegsiv345@gmail.com>
2023-06-17 00:45:00 +0300

    Major add dialogue system

---
+++ Siacode Project 2D/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using Ink.Runtime;
+using TMPro;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    private PlayerInputActions _playerInputActions;
+    private static DialogueManager instance;
+
+    [Header("Dialogue UI")] [SerializeField]
+    private GameObject dialoguePanel;
+
+    [SerializeField] private TextMeshProUGUI dialogueText;
+
+    private Story currentStory;
+    public bool dialogueIsPlaying { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerInputActions = new PlayerInputActions();
+        _playerInputActions.Enable();
+        dialogueIsPlaying = false;
+        dialoguePanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!dialogueIsPlaying)
+        {
+            return;
+        }
+
+        if (_playerInputActions.Player.Interact.triggered)
+        {
+            ContinueStory();
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("Больше одного инстанса Менеджера диалога в сцене!");
+        }
+
+        instance = this;
+    }
+
+    public static DialogueManager GetInstance()
+    {
+        return instance;
+    }
+
+    public void EnterDialogueMode(TextAsset inkJSON)
+    {
+        currentStory = new Story(inkJSON.text);
+        dialogueIsPlaying = true;
+        dialoguePanel.SetActive(true);
+
+        ContinueStory();
+    }
+
+    private IEnumerator ExitDialogueMode()
+    {
+        yield return new WaitForSeconds(0.2f);
+
+        dialogueIsPlaying = false;
+        dialoguePanel.SetActive(false);
+        dialogueText.text = "";
+    }
+
+    private void ContinueStory()
+    {
+        if (currentStory.canContinue)
+        {
+            dialogueText.text = currentStory.Continue();
+        }
+        else
+        {
+            StartCoroutine(ExitDialogueMode());
+        }
+    }
+}
