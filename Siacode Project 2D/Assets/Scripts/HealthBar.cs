be0b31e25a17d27b62a0058d892f894feb746300
Killisu <v.a.2003@yandex.ru>
2023-06-19 03:17:31 +0300

    edit health system

--- Siacode Project 2D/Assets/Scripts/HealthBar.cs
+++ Siacode Project 2D/Assets/Scripts/HealthBar.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class HealthBar : MonoBehaviour
 {
     public GameObject heartPrefab;
-    public float health, maxHealth;
+    public PlayerHealth playerHealth;
     List<HealthHeartBar> hearts = new List<HealthHeartBar>();
 
     public void DrawHearts()
     {
         ClearHearts();
-        float maxHealthRemainder = maxHealth % 2;
-        int heartsToMake = (int)((maxHealth / 2) + maxHealthRemainder);
+        float maxHealthRemainder = playerHealth.maxHealth % 2;
+        int heartsToMake = (int)((playerHealth.maxHealth / 2) + maxHealthRemainder);
         for(int i = 0; i < heartsToMake; i++)
         {
             CreateEmptyHeart();
+        }
+
+        for(int i = 0; i < hearts.Count; i++)
+        {
+            int heartStatusRemainder = (int)Mathf.Clamp(playerHealth.health - (i * 2), 0, 2);
+            hearts[i].SetHeartImage((HeartStatus)heartStatusRemainder);
         }
     }
 
@@ -35,6 +41,7 @@ public class HealthBar : MonoBehaviour
         {
             Destroy(t.gameObject);
         }
+        hearts = new List<HealthHeartBar>();
 
     }
 
